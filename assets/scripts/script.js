092b1ede210f05169b63ac0629660aaf06e8b165
DmitryRasov <letucepray@yandex.ru>
2021-09-16 13:19:22 +0300

    docs: added verification script & verification css

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,40 +1,20 @@
-// // Anchor navigation
-// document.querySelectorAll('a[href^="#"').forEach(link => {
-
-//     link.addEventListener('click', function(e) {
-//         e.preventDefault();
-
-//         let href = this.getAttribute('href').substring(1);
-
-//         const scrollTarget = document.getElementById(href);
-
-//         const topOffset = document.querySelector('.scrollto').offsetHeight;
-//         // const topOffset = 0; // если не нужен отступ сверху 
-//         const elementPosition = scrollTarget.getBoundingClientRect().top;
-//         const offsetPosition = elementPosition - topOffset;
-
-//         window.scrollBy({
-//             top: offsetPosition,
-//             behavior: 'smooth'
-//         });
-//     });
 // up button
 (function() {
     'use strict';
   
     function trackScroll() {
       var scrolled = window.pageYOffset;
-      var coords = document.documentElement.clientHeight / 6;
+      var coords = document.documentElement.clientHeight / 6; // скорость
   
       if (scrolled > coords) {
-        goTopBtn.classList.add('back_to_top-show');
+        goTopBtn.classList.add('back_to_top-show'); // условие отображения
       }
       if (scrolled < coords) {
-        goTopBtn.classList.remove('back_to_top-show');
+        goTopBtn.classList.remove('back_to_top-show'); // условие НЕ отображения))00)
       }
     }
   
-    function backToTop() {
+    function backToTop() { // подъём
       if (window.pageYOffset > 0) {
         window.scrollBy(0, -10);
         setTimeout(backToTop, 0);
